6719198fbb29a53a5b17bb747c78179ceb18fc08
Adrian Moses <adrian@adrianmoses.com>
2024-02-29 21:30:52 +0100

    explicitly set creds to false

--- next.config.js
+++ next.config.js
@@ -5,11 +5,10 @@ const nextConfig = {
       {
         source: "/api/graphql",
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "origin", value: "*"},
           { key: "Access-Control-Allow-Origin", value: "*" },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "*" },
+          { key: "Access-Control-Allow-Credentials", value: "false" }
         ]
       }
     ]
